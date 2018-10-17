9cfcab52c0150885cf691fd0f6492fc0d08adda0
JAXON <jaxon@jsk.imi.i.u-tokyo.ac.jp>
2018-10-17 18:31:50 +0900

    switch when x11 forwarded or on the local machine

--- run.sh
+++ run.sh
@@ -6,12 +6,28 @@ rossetip
 usb_arg=`python3 generate_usb_arg.py`
 echo $usb_arg
 
+if [ $DISPLAY == "localhost:10.0" ]; then
+    sudo docker run -it --rm \
+        --net=host \
+        --env="DISPLAY" \
+        --volume="${HOME}/.Xauthority:/root/.Xauthority:rw" \
+        -w="/home/leus" \
+        --net="host" \
+        --env="ROS_IP" \
+        --env="ROS_HOSTNAME" \
+        ${usb_arg} \
+    future731/pointgrey_test "$@"
+else
+    xhost +local:
+    sudo docker run -it --rm \
+        --net=host \
+        --env="DISPLAY" \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        -w="/home/leus" \
+        --net="host" \
+        --env="ROS_IP" \
+        --env="ROS_HOSTNAME" \
+        ${usb_arg} \
+    future731/pointgrey_test "$@"
+fi
 
-sudo docker run -it --rm \
-    --net=host \
-    -w="/home/leus" \
-    --net="host" \
-    --env="ROS_IP" \
-    --env="ROS_HOSTNAME" \
-    ${usb_arg} \
-future731/pointgrey_test "$@"
