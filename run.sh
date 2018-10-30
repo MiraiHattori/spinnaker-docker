7eaa832e3be35d0f31502a188429d1dc7c74fa28
JAXON <jaxon@jsk.imi.i.u-tokyo.ac.jp>
2018-10-30 21:42:34 +0900

    put "" in run.sh around env var

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 source /opt/ros/${ROS_DISTRO}/setup.bash
-rossetip
 
 usb_arg=`python3 generate_usb_arg.py`
 echo $usb_arg
 
-if [ $DISPLAY == "localhost:10.0" ]; then
+if [ "$DISPLAY" == "localhost:10.0" ]; then
     sudo docker run -it --rm \
         --net=host \
         --env="DISPLAY" \
