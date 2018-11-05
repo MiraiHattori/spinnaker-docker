ee24171e99c184d9fb988275b8e1789ac8039cc0
Mirai Hattori <m-hattori@jsk.t.u-tokyo.ac.jp>
2018-11-05 22:50:37 +0900

    rename launch file in my_entrypoint.sh

--- my_entrypoint.sh
+++ my_entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 
 # setup ros environment
 source "/home/leus/catkin_ws/devel/setup.bash"
-roslaunch ball_orbit_estimator stereo.launch
+roslaunch ball_orbit_estimator detect_ball_orbit.launch
 exec "$@"
